3a4d70d51a50a9514db43da753477d82374044e5
cpangam <cpangam@alumni.scu.edu>
2019-06-12 16:15:34 -0700

    Fixes for MR suggestion

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Malwarebytes Cloud app for splunk>phantom
+
+Malwarebytes protects people and businesses against dangerous threats such as malware, ransomware,
+and exploits that escape detection by traditional antivirus solutions.
+The product combines advanced heuristic threat detection with signature-less technologies to detect
+and stop a cyber attack before damage occurs.
+"""

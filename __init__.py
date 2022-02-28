49e20ae95cb661b7beadd551f7c632a4bdd5ff2b
Vishal Hirapara <vhirapara@splunk.com>
2022-02-28 12:09:25 +0530

    updated licence docs-string

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # File: __init__.py
 #
-# Copyright (c) Malwarebytes, 2022
+# Copyright (c) Malwarebytes, 2019-2022
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
